e63c56cd78e490a2a275ff6a480675d85fc9099d
Dave Hale <davejlhale@yahoo.co.uk>
2024-03-15 22:00:40 +0000

    init

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -36,73 +36,82 @@ export const Navbar = () => {
   );
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="w-40 sm:w-60 md:w-100">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink className={clsx(linkStyles({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium")} color="foreground" href={item.href}>
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+    <>
+      <NextUINavbar maxWidth="xl" position="sticky">
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+          {/* brand logo */}
+          <NavbarBrand as="li" className="w-40 sm:w-100 md:w-100">
+            <NextLink className="flex justify-start items-center gap-1" href="/">
+              <Logo />
+            </NextLink>
+          </NavbarBrand>
 
-      <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.sutranovumLinks.twitter} aria-label="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.sutranovumLinks.youtube} aria-label="Youtube">
-            <YoutubeIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.sutranovumLinks.instagram} aria-label="Instagram">
-            <InstagramIcon className="text-default-500" />
-          </Link>
-        </NavbarItem>
-        {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
-        <NavbarItem className="hidden sm:flex">
-          <Button isExternal as={Link} className="text-sm font-normal text-default-600 bg-default-100" href={siteConfig.links.goFundMe} startContent={<HeartFilledIcon className="text-danger" />} variant="flat">
-            Donate
-          </Button>
-          <ThemeSwitch />
-        </NavbarItem>
-      </NavbarContent>
+          {/* large screen nav links */}
+          <ul className="hidden lg:flex gap-4 justify-start ml-2">
+            {siteConfig.navItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <NextLink className={clsx(linkStyles({ color: "foreground" }), "data-[active=true]:text-primary data-[active=true]:font-medium")} color="foreground" href={item.href}>
+                  {item.label}
+                </NextLink>
+              </NavbarItem>
+            ))}
+          </ul>
+        </NavbarContent>
 
-      {/* small screen  */}
+        <NavbarContent className=" sm:flex basis-1/5 sm:basis-full" justify="end">
+          {/* search box */}
+          {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.sutranovumLinks.twitter} aria-label="Twitter">
-          <TwitterIcon className="text-default-500" />
-        </Link>
-        <Link isExternal href={siteConfig.sutranovumLinks.youtube} aria-label="Youtube">
-          <YoutubeIcon className="text-default-500" />
-        </Link>
-        <Link isExternal href={siteConfig.sutranovumLinks.instagram} aria-label="Instagram">
-          <InstagramIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
+          {/* theme toggle */}
+          <NavbarItem>
+            <ThemeSwitch />
+          </NavbarItem>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link color={index === 2 ? "primary" : index === siteConfig.navMenuItems.length - 1 ? "danger" : "foreground"} href="#" size="lg">
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
+          {/* menu toggle */}
+          <NavbarItem className="lg:hidden">
+            <NavbarMenuToggle className="w-6 h-6"></NavbarMenuToggle>
+          </NavbarItem>
+        </NavbarContent>
+
+        {/* small screen  */}
+        <NavbarMenu>
+          {searchInput}
+          <div className="mx-4 mt-2 flex flex-col gap-2">
+            {siteConfig.navMenuItems.map((item, index) => (
+              <NavbarMenuItem key={`${item}-${index}`}>
+                <Link color={index === 2 ? "primary" : index === siteConfig.navMenuItems.length - 1 ? "danger" : "foreground"} href="#" size="lg">
+                  {item.label}
+                </Link>
+              </NavbarMenuItem>
+            ))}
+          </div>
+        </NavbarMenu>
+      </NextUINavbar>
+
+      {/* row2 */}
+      <NextUINavbar maxWidth="xl" position="sticky">
+        <NavbarContent className=" flex basis-full" justify="center">
+          {/* donate button */}
+          <NavbarItem>
+            <Button isExternal as={Link} className="text-sm font-normal text-default-600 bg-default-100" href={siteConfig.links.goFundMe} startContent={<HeartFilledIcon className="text-danger" />} variant="flat">
+              Donate
+            </Button>
+          </NavbarItem>
+
+          {/* social media */}
+          <NavbarItem className="flex gap-2  bg-slate-50 text-center  ">
+            <Link isExternal href={siteConfig.sutranovumLinks.twitter} aria-label="Twitter" className="p-1 ">
+              <TwitterIcon className="text-default-500" />
+            </Link>
+            <Link isExternal href={siteConfig.sutranovumLinks.youtube} aria-label="Youtube" className="p-1 ">
+              <YoutubeIcon className="text-default-500 m-2" />
+            </Link>
+            <Link isExternal href={siteConfig.sutranovumLinks.instagram} aria-label="Instagram" className="p-1 ">
+              <InstagramIcon className="text-default-500" />
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </NextUINavbar>
+    </>
   );
 };
